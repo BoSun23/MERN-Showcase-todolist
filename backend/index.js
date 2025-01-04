d96db3cbbe7d3e3b238ff0cb5f4966b50fb75a4d
BoSun23 <sunboadrien@gmail.com>
2025-01-04 12:19:11 -0500

    forntedn ready for CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors =  require('cors')
+const TodoModel = require('./Models/Todo')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+
+mongoose.connect('mongodb+srv://frontend:MERNTodoList@mermtodolist.evhpy.mongodb.net/?retryWrites=true&w=majority&appName=MERMTodoList')
+
+app.post('/add',(req, res => {
+    const task = req.body.task;
+    TodoModel.create({
+        task:task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+}))
 
 app.listen(3001,() =>{
     console.log("Server is Running")
