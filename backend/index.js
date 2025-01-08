f5094d57031a9b1062acf7e4629bc7315eb80f57
BoSun23 <sunboadrien@gmail.com>
2025-01-08 16:24:55 -0500

    router

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,15 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const app = express();
 const mongoose = require('mongoose')
 const cors =  require('cors')
 const TodoModel = require('./Models/Todo')
-const path = require('path')
-
-const app = express()
 app.use(cors())
 app.use(express.json())
 
 
 mongoose.connect('mongodb+srv://frontend:g2qWi91XudwbhB2P@mermtodolist.evhpy.mongodb.net/?retryWrites=true&w=majority&appName=MERMTodoList')
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/get',(req,res) => {
